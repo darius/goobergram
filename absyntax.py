5ffcf54fa5757087a0ff19c304660802719aabdb
Darius Bacon <darius@wry.me>
2016-05-25 21:33:31 -0400

    pack context into an Environment

--- absyntax.py
+++ absyntax.py
@@ -8,13 +8,16 @@ from structs import Struct
 import linear_constraints as LC
 
 def run(program):
-    env = {'number': NumberType()}
-    type_ = Type(Definition('<program>', None, program),
-                 env)
+    types = {'number': NumberType()}
+    env = Environment(types, None)
+    type_ = Type(Definition('<program>', None, program))
     inst = type_.instantiate(env, ())
     for part in inst.get_parts():
-        part.draw()
+        part.draw(env)
 
+class Environment(Struct('types inst')):
+    def spawn(self, inst):
+        return Environment(self.types, inst)
 
 class NumberType(object):
     def instantiate(self, env, params):
@@ -22,18 +25,19 @@ class NumberType(object):
         return NumberInstance()
 
 class NumberInstance(LC.Number):
-    def draw(self):
+    def draw(self, env):
         pass
 
-class Type(Struct('defn env')):
+class Type(Struct('defn')):     # XXX fold into Definition
     def instantiate(self, env, params):
         inst = Instance(self)
-        self.defn.populate(inst, env)
+        subenv = env.spawn(inst) # XXX won't see global vars
+        self.defn.populate(subenv)
         for id, expr in params:
             assert False, 'XXX'
         return inst
-    def draw(self, inst):
-        self.defn.draw(inst, self.env)
+    def draw(self, env):
+        self.defn.draw(env)
     def __repr__(self):
         return '<type %s>' % self.defn.id
 
@@ -42,11 +46,11 @@ class TupleType(Struct('fields')):
         inst = Instance(self)
         for f in self.fields:
             inst.mapping[f] = NumberInstance()
+        subenv = env.spawn(inst)
         for f, expr in params:
-            LC.equate(inst.mapping[f], expr.evaluate(None, # XXX
-                                                     env))
+            LC.equate(inst.mapping[f], expr.evaluate(subenv))
         return inst
-    def draw(self, inst):
+    def draw(self, env):
         pass
     def __repr__(self):
         return '<tuple-type %s>' % ','.join(self.fields)
@@ -55,73 +59,73 @@ class Instance(LC.Compound):
     def __init__(self, type_):
         LC.Compound.__init__(self, {})
         self.type = type_
-    def draw(self):
-        self.type.draw(self)
+    def draw(self, env):
+        self.type.draw(env.spawn(self))
     def __repr__(self):
         return '<instance of %r: %r>' % (self.type, sorted(self.keys()))
 
 
 class Definition(Struct('id extends decls')):
-    def build(self, inst, env):
-        env[self.id] = Type(self, env)
-    def populate(self, inst, env):
+    def build(self, env):
+        env.types[self.id] = Type(self)
+    def populate(self, env):
         supe = self.super_definition(env)
         if supe:
-            supe.populate(inst, env)  # XXX right env?
+            supe.populate(env)
         for decl in self.decls:
-            decl.build(inst, env)
-    def draw(self, inst, env):
+            decl.build(env)
+    def draw(self, env):
         supe = self.super_definition(env)
         if supe:
-            supe.draw(inst, env) # XXX does the user always want this?
+            supe.draw(env) # XXX does the user always want this?
         for decl in self.decls:
-            decl.draw(inst, env)
-    def super_definition(self, env): # XXX use env from defn time, I guess
+            decl.draw(env)
+    def super_definition(self, env):
         if self.extends:
-            supertype = env[self.extends]
+            supertype = env.types[self.extends]
             assert isinstance(supertype, Type), "%r is not a type" % supertype
             return supertype.defn
         else:
             return None
 
 class VarDecl(Struct('type_id decls')):
-    def build(self, inst, env):
-        type_ = env[self.type_id]
+    def build(self, env):
+        type_ = env.types[self.type_id]
         assert isinstance(type_, (Type, NumberType)), "%r is not a type" % type_
         for decl in self.decls:
-            inst.mapping[decl.id] = type_.instantiate(env, decl.params)
-    def draw(self, inst, env):
+            env.inst.mapping[decl.id] = type_.instantiate(env, decl.params)
+    def draw(self, env):
         pass
 
 Declarator = Struct('id params', name='Declarator')
 
 class Constraints(Struct('equations')):
-    def build(self, inst, env):
+    def build(self, env):
         for lhs, rhs in self.equations:
-            LC.equate(lhs.evaluate(inst, env), rhs.evaluate(inst, env))
-    def draw(self, inst, env):
+            LC.equate(lhs.evaluate(env), rhs.evaluate(env))
+    def draw(self, env):
         pass
 
 class Draw(Struct('drawables')):
-    def build(self, inst, env):
+    def build(self, env):
         pass
-    def draw(self, inst, env):
+    def draw(self, env):
         for drawable in self.drawables:
-            drawable.draw(inst, env)
+            drawable.draw(env)
 
 class DrawFunction(Struct('fn_name')):
-    def draw(self, inst, env):
-        draw_functions[self.fn_name](**inst.mapping)
+    def draw(self, env):
+        draw_functions[self.fn_name](**env.inst.mapping)
 
 class DrawName(Struct('name')):
-    def draw(self, inst, env):
-        self.name.evaluate(inst, env).draw()
+    def draw(self, env):
+        self.name.evaluate(env).draw()
 
 
 class BinaryOp(Struct('arg1 arg2')):
-    def evaluate(self, inst, env):
-        return self.operate(self.arg1.evaluate(inst, env),
-                            self.arg2.evaluate(inst, env))
+    def evaluate(self, env):
+        return self.operate(self.arg1.evaluate(env),
+                            self.arg2.evaluate(env))
 
 class Add(BinaryOp):
     operate = operator.add
@@ -140,23 +144,23 @@ def Negate(expr):
 
 
 class Tuple(Struct('exprs')):
-    def evaluate(self, inst, env):
+    def evaluate(self, env):
         assert len(self.exprs) in tuple_types
         params = zip('xyz', self.exprs)
         return tuple_types[len(self.exprs)].instantiate(env, params)
 
 class Name(Struct('id')):
-    def evaluate(self, inst, env):
-        if self.id not in inst.mapping:
-            assert 0, "not found: %r in %r" % (self.id, inst)
-        return inst.mapping[self.id] # XXX what about env?
+    def evaluate(self, env):
+        if self.id not in env.inst.mapping:
+            assert 0, "not found: %r in %r" % (self.id, env.inst)
+        return env.inst.mapping[self.id]
 
 class Dot(Struct('base field')):
-    def evaluate(self, inst, env):
-        return self.base.evaluate(inst, env).mapping[self.field]
+    def evaluate(self, env):
+        return self.base.evaluate(env).mapping[self.field]
 
 class Number(Struct('value')):
-    def evaluate(self, inst, env):
+    def evaluate(self, env):
         return self.value
 
 tuple_types = {
